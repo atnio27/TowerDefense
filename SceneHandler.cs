47e570f40dffe938fc2740f8e73375a6d7ee38c5
Antonio Esclapez <titoway33@gmail.com>
2024-05-03 09:34:03 +0200

    3 mayo
    Aspecto del cursor cambiado

--- SceneHandler.cs
+++ SceneHandler.cs
@@ -3,11 +3,6 @@ using System;
 
 public class SceneHandler : Node
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GetNode<Control>("MainMenu/M/VB/NewGame").Connect("pressed", this, "OnNewGamePressed");
@@ -16,17 +11,14 @@ public class SceneHandler : Node
 	
 	public void OnNewGamePressed() 
 	{
-		
+		GetNode<Control>("MainMenu").QueueFree();
+		PackedScene GameScene = (PackedScene)ResourceLoader.Load("res://Scenes/MainScenes/GameScene.tscn");
+		Node GameInstance = GameScene.Instance();
+		AddChild(GameInstance);
 	}
 	
 	public void OnQuitPressed()
 	{
 		GetTree().Quit();
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
