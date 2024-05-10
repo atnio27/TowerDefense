dc3802b2a97ad27baa1609237f4fe5d096af376e
Antonio Esclapez <titoway33@gmail.com>
2024-05-10 09:52:58 +0200

    10 Mayo
    Indicador de rango implementado, en proceso la implementación del singleton de las torretas

--- Scenes/MainScenes/UI.cs
+++ Scenes/MainScenes/UI.cs
@@ -1,16 +1,30 @@
 using Godot;
 using System;
+using System.Numerics;
 
 public class UI : CanvasLayer
 {
-	public void SetTowerPreview(string towerType, Vector2 mousePosition)
+	float towerRange = 1000;
+	public void SetTowerPreview(string towerType, Godot.Vector2 mousePosition)
 	{
 		PackedScene dragTower = (PackedScene)ResourceLoader.Load("res://Scenes/Turrets/" + towerType + ".tscn");
 		Node dragTowerInstance = dragTower.Instance();
 		dragTowerInstance.Name = "DragTower";
 
+		Sprite rangeTexture = new Sprite
+		{
+			Position = new Godot.Vector2(32, 32)
+		};
+		float scaling = towerRange / 600.0f;
+		rangeTexture.Scale = new Godot.Vector2(scaling, scaling);
+		Texture texture = (Texture)ResourceLoader.Load("res://Assets/UI/range_overlay.png");
+		rangeTexture.Texture = texture;
+		rangeTexture.Modulate = new Color("ad54ff3c");
+
+
 		Control control = new Control();
 		control.AddChild(dragTowerInstance, true);
+		control.AddChild(rangeTexture, true);
 		control.RectPosition = mousePosition;
 		control.Name = "TowerPreview";
 		AddChild(control, true);
@@ -19,12 +33,13 @@ public class UI : CanvasLayer
 		GetNode<Node2D>("TowerPreview/DragTower").Modulate = new Color("ad54ff3c");
 	}
 	
-	public void updateTowerPreview(Vector2 newPosition, Color color)
+	public void updateTowerPreview(Godot.Vector2 newPosition, Color color)
 	{
 		GetNode<Control>("TowerPreview").RectPosition = newPosition;
 		if (GetNode<Node2D>("TowerPreview/DragTower").Modulate != color)
 		{
 			GetNode<Node2D>("TowerPreview/DragTower").Modulate = color;
+			GetNode<Node2D>("TowerPreview/Sprite").Modulate = color;
 		}
 	}
 }
---
+++ Singletones/GameData.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class new_script : Node
+{
+    //  Dictionary<string, Dictionary<string, string>> towerData = new Dictionary<string, Dictionary<string, string>>();
+}
